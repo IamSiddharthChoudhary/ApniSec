bed179e0881e6f41837fd46b7157a4e0f7707d59
Siddharth Choudhary <csiddharth380@gmail.com>
2025-12-25 00:56:55 +0530

    Completed Auth with 6 apis

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Auth } from "../../utils/authClass";
+
+const auth = new Auth();
+
+export async function POST(req: NextRequest) {
+  try {
+    const r = await req.json();
+    const { email, password } = r;
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "Missing required fields" },
+        { status: 400 }
+      );
+    }
+
+    const { user, tk } = await auth.login(email, password);
+
+    return NextResponse.json(
+      { token: tk, user: user, message: "Successful" },
+      { status: 200 }
+    );
+  } catch (e) {
+    return NextResponse.json({ error: e }, { status: 400 });
+  }
+}
