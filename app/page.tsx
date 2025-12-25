02d7d2cc4cd4af81398e6608d425dc8d49dcd8f6
Siddharth Choudhary <csiddharth380@gmail.com>
2025-12-25 07:43:13 +0530

    Added components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
+import Features from "@/componenrs/features";
+import Footer from "@/componenrs/footer";
 import Hero from "@/componenrs/hero";
+import LiveIssues from "@/componenrs/live";
+import Services from "@/componenrs/services";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Features />
+      <LiveIssues />
+      <Services />
+      <Footer />
+    </>
+  );
 }
